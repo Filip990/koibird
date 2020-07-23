ead47b6708cf4b0fcbc75bce015e3cb28df7d5bd
Filip990 <filipfilipovic.stray@gmail.com>
2020-07-23 17:24:58 +0200

    halfway there

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import "./index.scss";
 
 //Imports for webpack
 import headerjs from "./components/header/header";
+import cardjs from "./components/card/card";
 //End of imports for webpack
 
 import header from "./components/header/header.html";
 import banner from "./components/banner/banner.html";
+import card from "./components/card/card.html";
 
 document.body.innerHTML = `
   ${header}
   ${banner}
+  ${card}
 `;
---
+++ src/sliderBuilder.js
@@ -0,0 +1,22 @@
+export function buildSlider(container, data) {
+	const cardsContainer = document.getElementById(container);
+
+	data.forEach((article) => {
+		const card = document.createElement("div");
+		const img = document.createElement("img");
+		const name = document.createElement("p");
+		const price = document.createElement("div");
+		const btn = document.createElement("button");
+
+		card.className = "card";
+		btn.className = "shop-now";
+
+		img.src = article.image;
+		name.innerHTML += article.name;
+		price.innerHTML += `&#8356; ${article.price}`;
+		btn.innerHTML += "Shop now";
+
+		card.append(img, name, price, btn);
+		cardsContainer.appendChild(card);
+	});
+}
