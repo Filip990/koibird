58c1960b9aee9a6447873baae1d2d73d02dfc862
Filip990 <filipfilipovic.stray@gmail.com>
2020-07-23 12:30:40 +0200

    added banner section

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import headerjs from "./components/header/header";
 //End of imports for webpack
 
 import header from "./components/header/header.html";
+import banner from "./components/banner/banner.html";
 
 document.body.innerHTML = `
   ${header}
+  ${banner}
 `;
