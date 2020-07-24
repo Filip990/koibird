a2c09e2f94fa1b0bb669a43ad11a51f9d3c26c8f
Filip990 <filipfilipovic.stray@gmail.com>
2020-07-24 16:24:10 +0200

    added best sellers section

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import "./index.scss";
 import headerjs from "./components/header/header";
 import newCollectionjs from "./components/newCollection/newCollection";
 import categoriesjs from "./components/categories/categories";
+import bestSellersjs from "./components/bestSellers/bestSellers";
 //End of imports for webpack
 
 import header from "./components/header/header.html";
 import banner from "./components/banner/banner.html";
 import newCollection from "./components/newCollection/newCollection.html";
 import categories from "./components/categories/categories.html";
-
+import bestSellers from "./components/bestSellers/bestSellers.html";
 document.body.innerHTML = `
   ${header}
   ${banner}
   ${newCollection}
   ${categories}
+  ${bestSellers}
 `;
