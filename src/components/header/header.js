72fd3c04364ccdf4934f804525d8bd684ef3d26f
Filip990 <filipfilipovic.stray@gmail.com>
2020-07-26 13:10:36 +0200

    reorganized

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,6 @@
 window.toggleMenu = () => {
-	var x = document.getElementById("header-links");
+	const x = document.getElementById("header-links");
+
 	if (x.className === "topnav") {
 		x.className += " visible";
 	} else {
--- src/components/promo/promo.js
+++ src/components/promo/promo.js
@@ -1,11 +1,9 @@
 import data from "../../data.json";
 
-const promoImages = data.promo.map((img) => img);
-
 window.addEventListener("load", function () {
 	const container = document.getElementById("promo-container");
 
-	promoImages.forEach((item) => {
+	data.promo.forEach((item) => {
 		const img = document.createElement("div");
 
 		img.style.backgroundImage = `url(${item.image})`;
